f3851554f6182816b930269a89b899d26cd67d61
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-04-22 15:31:51 -0400

    fixed AttrDriver.java

--- Attr/src/edu/saintjoe/cs/brianc/attr/AttrDriver.java
+++ Attr/src/edu/saintjoe/cs/brianc/attr/AttrDriver.java
@@ -1,12 +1,16 @@
 package edu.saintjoe.cs.brianc.attr;
 
 public class AttrDriver {
-	public Attr one;
-	public Attr two;
-	public Attr three;
 	
 	public static void main(String[] args) {
 		
-	one = new Attr("Name", "Brian");
+	Attr one;
+	Attr two;
+	
+	one = new Attr("Name", "Jared");
+	two = new Attr("Height", 60);
+	
+	System.out.println(one.toString());
+	System.out.println(two.toString());
 	}
 }
