095b1ae4a5050f514de94431eba173b4828efad9
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-04-24 15:51:35 -0400

    Added Driver file

---
+++ Attr/src/edu/saintjoe/cs/brianc/attr/ColorAttrDriver.java
@@ -0,0 +1,12 @@
+package edu.saintjoe.cs.brianc.attr;
+
+public class ColorAttrDriver {
+	
+	public static void main (String [] args) {
+	
+		ColorAttr testColor;
+	
+		testColor = new ColorAttr("Color", "red");
+		System.out.println(testColor.toString());
+	}
+}
