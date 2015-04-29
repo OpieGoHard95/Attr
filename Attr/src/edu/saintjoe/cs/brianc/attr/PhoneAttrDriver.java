68e3479e6ae880688e050fc6f573008beaef55b9
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-04-29 15:00:17 -0400

    Finished the PhoneAttrDriver file

--- Attr/src/edu/saintjoe/cs/brianc/attr/PhoneAttrDriver.java
+++ Attr/src/edu/saintjoe/cs/brianc/attr/PhoneAttrDriver.java
@@ -3,6 +3,13 @@ package edu.saintjoe.cs.brianc.attr;
 public class PhoneAttrDriver {
 	public static void main(String [] args) {
 		PhoneAttr testPhone = new PhoneAttr();
+		System.out.println(testPhone.toString());
+		PhoneAttr iphone6 = new PhoneAttr("Apple", "iPhone6", 2);
+		System.out.println(iphone6.toString());
+		PhoneAttr galaxys6 = new PhoneAttr("Samsung", "Galaxy S6");
+		System.out.println(galaxys6.toString());
+		PhoneAttr Android = new PhoneAttr("Android");
+		System.out.println(Android.toString());
 	}
 
 }
